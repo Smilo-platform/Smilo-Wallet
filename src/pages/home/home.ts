97f08af1a6a741d2476e67c31a56e0cad2069ec4
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-01 11:49:42 +0200

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor() {
+    
+  }
+
+}
