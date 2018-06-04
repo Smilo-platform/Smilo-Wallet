dbe265493833199362d1ea8caaa614487e967aec
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-04 15:35:09 +0200

    All pages are now imported correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { SmiloWallet } from "./app.component";
 import { HomePageModule } from "../pages/home/home.module";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { AboutPageModule } from "../pages/about/about.module";
+import { WalletOverviewPageModule } from "../pages/wallet-overview/wallet-overview.module";
+import { WalletPageModule } from "../pages/wallet/wallet.module";
+import { FaqPageModule } from "../pages/faq/faq.module";
+import { SettingsGeneralPageModule } from "../pages/settings-general/settings-general.module";
+import { SettingsWalletPageModule } from "../pages/settings-wallet/settings-wallet.module";
+import { TransferPageModule } from "../pages/transfer/transfer.module";
+import { WalletImportKeystorePageModule } from "../pages/wallet-import-keystore/wallet-import-keystore.module";
+import { WalletImportLedgerPageModule } from "../pages/wallet-import-ledger/wallet-import-ledger.module";
+import { WalletImportPrivatekeyPageModule } from "../pages/wallet-import-privatekey/wallet-import-privatekey.module";
+import { RestoreBackupPageModule } from "../pages/restore-backup/restore-backup.module";
+import { CryptoKeyService } from "../services/crypto-key-service";
+import { LandingPageModule } from "../pages/landing/landing.module";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -29,6 +42,18 @@ export function HttpLoaderFactory(http: HttpClient) {
       }
     }),
     HomePageModule,
+    AboutPageModule,
+    WalletOverviewPageModule,
+    WalletPageModule,
+    FaqPageModule,
+    SettingsGeneralPageModule,
+    SettingsWalletPageModule,
+    TransferPageModule,
+    WalletImportKeystorePageModule,
+    WalletImportLedgerPageModule,
+    WalletImportPrivatekeyPageModule,
+    RestoreBackupPageModule,
+    LandingPageModule,
     IonicModule.forRoot(SmiloWallet)
   ],
   bootstrap: [IonicApp],
@@ -38,6 +63,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [
     StatusBar,
     SplashScreen,
+    CryptoKeyService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
