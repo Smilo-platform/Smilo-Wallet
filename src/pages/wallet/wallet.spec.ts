d5042e7d7f8be73e085582be8704e762be55db49
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-04 16:38:22 +0200

    The import wallet page now links to the correct pages

--- src/pages/wallet/wallet.spec.ts
+++ src/pages/wallet/wallet.spec.ts
@@ -3,19 +3,31 @@ import { WalletPage } from "./wallet";
 import { IonicModule, NavController, NavParams} from "ionic-angular/index";
 import { MockNavController } from "../../../test-config/mocks/MockNavController";
 import { MockNavParams } from "../../../test-config/mocks/MockNavParams";
+import { WalletImportKeystorePage } from "../wallet-import-keystore/wallet-import-keystore";
+import { WalletImportPrivatekeyPage } from "../wallet-import-privatekey/wallet-import-privatekey";
+import { WalletImportLedgerPage } from "../wallet-import-ledger/wallet-import-ledger";
+import { RestoreBackupPage } from "../restore-backup/restore-backup";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { MockTranslationLoader } from "../../../test-config/mocks/MockTranslationLoader";
 
 describe("WalletPage", () => {
   let comp: WalletPage;
   let fixture: ComponentFixture<WalletPage>;
+  let navController: NavController;
 
   beforeEach(async(() => {
+    navController = new MockNavController();
+
     TestBed.configureTestingModule({
       declarations: [WalletPage],
       imports: [
-        IonicModule.forRoot(WalletPage)
+        IonicModule.forRoot(WalletPage),
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: MockTranslationLoader},
+        })
       ],
       providers: [
-        { provide: NavController, useClass: MockNavController },
+        { provide: NavController, useValue: navController },
         { provide: NavParams, useValue: new MockNavParams() }
       ]
     }).compileComponents();
@@ -26,5 +38,37 @@ describe("WalletPage", () => {
     comp = fixture.componentInstance;
   });
 
-  it("should create component", () => expect(comp).toBeDefined())
+  it("should create component", () => expect(comp).toBeDefined());
+
+  it("should open the import keystore file page correctly", () => {
+    spyOn(navController, "push");
+
+    comp.openImportKeystorePage();
+
+    expect(navController.push).toHaveBeenCalledWith(WalletImportKeystorePage);
+  });
+
+  it("should open the import privatekey pagecorrectly", () => {
+    spyOn(navController, "push");
+
+    comp.openImportPrivatekeyPage();
+
+    expect(navController.push).toHaveBeenCalledWith(WalletImportPrivatekeyPage);
+  });
+
+  it("should open the import ledger page correctly", () => {
+    spyOn(navController, "push");
+
+    comp.openImportLedgerPage();
+
+    expect(navController.push).toHaveBeenCalledWith(WalletImportLedgerPage);
+  });
+
+  it("should open the restore backup page correctly", () => {
+    spyOn(navController, "push");
+
+    comp.openRestoreBackupPage();
+
+    expect(navController.push).toHaveBeenCalledWith(RestoreBackupPage);
+  });
 });
--- src/pages/wallet/wallet.ts
+++ src/pages/wallet/wallet.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { WalletImportKeystorePage } from "../wallet-import-keystore/wallet-import-keystore";
+import { WalletImportPrivatekeyPage } from "../wallet-import-privatekey/wallet-import-privatekey";
+import { WalletImportLedgerPage } from "../wallet-import-ledger/wallet-import-ledger";
+import { RestoreBackupPage } from "../restore-backup/restore-backup";
 
 /**
  * Generated class for the WalletPage page.
@@ -18,8 +22,20 @@ export class WalletPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad WalletPage");
+  openImportKeystorePage() {
+    this.navCtrl.push(WalletImportKeystorePage);
+  }
+
+  openImportPrivatekeyPage() {
+    this.navCtrl.push(WalletImportPrivatekeyPage);
+  }
+
+  openImportLedgerPage() {
+    this.navCtrl.push(WalletImportLedgerPage);
+  }
+
+  openRestoreBackupPage() {
+    this.navCtrl.push(RestoreBackupPage);
   }
 
 }
