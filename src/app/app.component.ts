b93d0ceab0157390e98745ae574deddd10262deb
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-04 13:25:43 +0200

    Added ngx-translate for i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
+import { TranslateService } from "@ngx-translate/core";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class SmiloWallet {
   rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform, 
+              private statusBar: StatusBar, 
+              private splashScreen: SplashScreen,
+              private translate: TranslateService) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
+
+      translate.setDefaultLang("en");
+      translate.use("en");
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
-import { SmiloWallet } from './app.component';
-import { HomePageModule } from '../pages/home/home.module';
+import { SmiloWallet } from "./app.component";
+import { HomePageModule } from "../pages/home/home.module";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -13,6 +20,14 @@ import { HomePageModule } from '../pages/home/home.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     HomePageModule,
     IonicModule.forRoot(SmiloWallet)
   ],
