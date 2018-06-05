d8c1d2a77130b7a26c251d8947f9bfd061c0e133
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-05 10:50:22 +0200

    Added the TranslateModule to the passphrase page

--- src/pages/wallet-new-passphrase/wallet-new-passphrase.module.ts
+++ src/pages/wallet-new-passphrase/wallet-new-passphrase.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WalletNewPassphrasePage } from './wallet-new-passphrase';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { WalletNewPassphrasePage } from './wallet-new-passphrase';
   ],
   imports: [
     IonicPageModule.forChild(WalletNewPassphrasePage),
+    TranslateModule
   ],
 })
 export class WalletNewPassphrasePageModule {}
