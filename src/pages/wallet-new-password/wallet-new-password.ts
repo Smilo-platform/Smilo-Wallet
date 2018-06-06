30c18a769719a710b5843b4c4d4d9258a4f0f01a
mathyn <mathyn@MacBook-Pro-van-mathyn.local>
2018-06-06 10:27:12 +0200

    Finished the new wallet password page

--- src/pages/wallet-new-password/wallet-new-password.ts
+++ src/pages/wallet-new-password/wallet-new-password.ts
@@ -1,25 +1,38 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the WalletNewPasswordPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { WalletNewDisclaimerPage } from "../wallet-new-disclaimer/wallet-new-disclaimer";
 
 @IonicPage()
 @Component({
-  selector: 'page-wallet-new-password',
-  templateUrl: 'wallet-new-password.html',
+  selector: "page-wallet-new-password",
+  templateUrl: "wallet-new-password.html",
 })
 export class WalletNewPasswordPage {
 
+  enteredPassword: string = "";
+  validatedPassword: string = "";
+
+  passphrase: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // Read the passphrase
+    this.passphrase = this.navParams.get("passphrase");
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad WalletNewPasswordPage');
+  next() {
+    if(!this.passwordsArePristine() && this.passwordsAreValid()) {
+      this.navCtrl.push(WalletNewDisclaimerPage, {
+        passphrase: this.passphrase,
+        password: this.enteredPassword
+      });
+    }
   }
 
+  passwordsArePristine() {
+    return this.enteredPassword.length == 0 || this.validatedPassword.length == 0;
+  }
+
+  passwordsAreValid() {
+    return this.enteredPassword == this.validatedPassword;
+  }
 }
