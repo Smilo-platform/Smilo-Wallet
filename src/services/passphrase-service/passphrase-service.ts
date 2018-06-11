6042cb72e15663b61c1666b3de503a642476c38c
Unknown <mail@mathyn.com>
2018-06-11 17:19:01 +0200

    Started working on implementing BIP39

--- src/services/passphrase-service/passphrase-service.ts
+++ src/services/passphrase-service/passphrase-service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from "@angular/core";
 
+declare var forge: any;
+
+export const PASSPHRASE_ENTROPY = 128;
+
 export interface IPassphraseService {
     generate(wordCount: number): string[];
 
@@ -10,6 +14,11 @@ export interface IPassphraseService {
 
 @Injectable()
 export class PassphraseService implements IPassphraseService {
+    /**
+     * The active word list. This will be loaded 'on the fly'
+     */
+    private activeWordList: string[];
+
     passphraseStringToWords(passphrase: string): string[] {
         let words = passphrase.split(" ");
 
@@ -23,6 +32,16 @@ export class PassphraseService implements IPassphraseService {
     }
 
     generate(wordCount: number): string[] {
+        // Right now we support 12 words with an entropy of 128 bits
+        // 128 bits means we use 8 bytes.
+
+        let checkSumSize = PASSPHRASE_ENTROPY / 32;
+
+        let entropy = forge.random.getBytesSync(PASSPHRASE_ENTROPY / 8);
+
+        // Get the checksum
+        
+
         let words: string[] = [];
 
         for(let i = 0; i < wordCount; i++) {
