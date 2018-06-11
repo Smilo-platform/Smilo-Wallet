c5374dbb2900335f417ae6607e57030b0c91d816
Unknown <mail@mathyn.com>
2018-06-11 10:51:08 +0200

    The passphrase service is now capable of validating a passphrase

--- src/services/passphrase-service/passphrase-service.ts
+++ src/services/passphrase-service/passphrase-service.ts
@@ -2,10 +2,26 @@ import { Injectable } from "@angular/core";
 
 export interface IPassphraseService {
     generate(wordCount: number): string[];
+
+    passphraseStringToWords(passphrase: string): string[];
+
+    isValid(passphrase: string, expectedWordCount: number): boolean;
 }
 
 @Injectable()
 export class PassphraseService implements IPassphraseService {
+    passphraseStringToWords(passphrase: string): string[] {
+        let words = passphrase.split(" ");
+
+        return words.filter(x => x.trim().length > 0);
+    }
+
+    isValid(passphrase: string, expectedWordCount: number): boolean {
+        let words = this.passphraseStringToWords(passphrase);
+
+        return words.length == expectedWordCount;
+    }
+
     generate(wordCount: number): string[] {
         let words: string[] = [];
 
