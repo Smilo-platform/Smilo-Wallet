da7f59e8a00933f294c8095aa8b222b9f2b4e541
Unknown <mail@mathyn.com>
2018-06-14 16:39:55 +0200

    Android release builds are now properly signed

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -5,4 +5,19 @@ ionic cordova build android --prod --release
 mkdir -p ./release
 
 # Move the APK to the release folder
-mv ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./release/android.apk
+mv ./platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk ./release/android-unsigned.apk
+
+# Remove any previous builds
+[ -e ./release/android.apk ] && rm ./release/android.apk
+
+# Sign the application
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $SMILO_WALLET_KEYSTORE_LOCATION ./release/android-unsigned.apk smilo-wallet
+
+# Zipalign
+zipalign -v 4 ./release/android-unsigned.apk ./release/android.apk
+
+# Ensure the signing went ok
+apksigner verify ./release/android.apk
+
+# Clean up
+rm ./release/android-unsigned.apk
