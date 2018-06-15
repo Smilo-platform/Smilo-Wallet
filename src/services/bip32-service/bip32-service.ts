17df7e6fba37fabaaae4327ee2d6cde5fc8260c1
Unknown <mail@mathyn.com>
2018-06-15 10:49:53 +0200

    Started working on the BIP 32 service

---
+++ src/services/bip32-service/bip32-service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+export interface IBIP32Account {
+    
+}
+
+export interface IBIP32Service {
+
+}
+
+@Injectable()
+export class BIP32Service implements IBIP32Service {
+
+}
