597335aeb2507cef297b82a3c133290718bd4b53
Unknown <mail@mathyn.com>
2018-07-02 10:52:18 +0200

    Removed unused dependency

--- src/pages/wallet-extra-import/wallet-extra-import.ts
+++ src/pages/wallet-extra-import/wallet-extra-import.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams, ViewController } from "ionic-angular";
+import { IonicPage, NavParams, ViewController } from "ionic-angular";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { WalletIndexValidator } from "../../validators/WalletIndexValidator";
 
