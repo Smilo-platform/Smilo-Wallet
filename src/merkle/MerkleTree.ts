9c8eecf042317ee9a584e833fe3cc21821df160c
Unknown <mail@mathyn.com>
2018-07-03 17:15:44 +0200

    The LamportGenerator now only uses sjcl

--- src/merkle/MerkleTree.ts
+++ src/merkle/MerkleTree.ts
@@ -193,7 +193,7 @@ export class MerkleTree {
             if(platform.is("android")) {
                 // Android requires the scripts to be loaded as shown below.
                 scripts = [
-                    `file:///android_asset/www/assets/scripts/forge.min.js`,
+                    // `file:///android_asset/www/assets/scripts/forge.min.js`,
                     `file:///android_asset/www/assets/scripts/sjcl.js`,
                     `file:///android_asset/www/assets/scripts/seedrandom.min.js`
                 ];
@@ -201,7 +201,7 @@ export class MerkleTree {
             else {
                 // Web & ios is easy.
                 scripts = [
-                    `${ window.location.protocol }//${ window.location.host }/assets/scripts/forge.min.js`,
+                    // `${ window.location.protocol }//${ window.location.host }/assets/scripts/forge.min.js`,
                     `${ window.location.protocol }//${ window.location.host }/assets/scripts/sjcl.js`,
                     `${ window.location.protocol }//${ window.location.host }/assets/scripts/seedrandom.min.js`,
                 ];
