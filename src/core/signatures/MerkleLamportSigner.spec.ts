b8153939888df8412e99e1982300dde488563fc5
Unknown <mail@mathyn.com>
2018-07-11 14:31:57 +0200

    Extended MerkleLamportSigner unit tests to be more correct

--- src/core/signatures/MerkleLamportSigner.spec.ts
+++ src/core/signatures/MerkleLamportSigner.spec.ts
@@ -5,7 +5,7 @@ interface IMerkleLamportSignerTestVector {
     name: string;
     input: {
         bitCount: number;
-        lamportPrivateKeys: string[];
+        lamportPrivateKeys: string[][];
         merkleTree: MerkleTree;
         message: string;
         privateKey: string;
@@ -28,25 +28,62 @@ describe("MerkleLamportSigner", () => {
             input: {
                 bitCount: 4,
                 lamportPrivateKeys: [
-                    "p1-1", "p1-2", "p2-1", "p2-2",
-                    "p3-1", "p3-2", "p4-1", "p4-2"
+                    [
+                        "i0p1", "i0p2", "i0p3", "i0p4",
+                        "i0p5", "i0p6", "i0p7", "i0p8"
+                    ],
+                    [
+                        "i1p1", "i1p2", "i1p3", "i1p4",
+                        "i1p5", "i1p6", "i1p7", "i1p8"
+                    ],
+                    [
+                        "i2p1", "i2p2", "i2p3", "i2p4",
+                        "i2p5", "i2p6", "i2p7", "i2p8"
+                    ],
+                    [
+                        "i3p1", "i3p2", "i3p3", "i3p4",
+                        "i3p5", "i3p6", "i3p7", "i3p8"
+                    ],
+                    [
+                        "i4p1", "i4p2", "i4p3", "i4p4",
+                        "i4p5", "i4p6", "i4p7", "i4p8"
+                    ],
+                    [
+                        "i5p1", "i5p2", "i5p3", "i5p4",
+                        "i5p5", "i5p6", "i5p7", "i5p8"
+                    ],
+                    [
+                        "i6p1", "i6p2", "i6p3", "i6p4",
+                        "i6p5", "i6p6", "i6p7", "i6p8"
+                    ],
+                    [
+                        "i7p1", "i7p2", "i7p3", "i7p4",
+                        "i7p5", "i7p6", "i7p7", "i7p8"
+                    ]
                 ],
                 merkleTree: new MerkleTree([
                     [
-                        "1", "2", "3", "4", "5", "6", "7", "8"
+                        "h5Y8vedTt03whfNJYmUDM00bExRkrIuZVMxbGLNYDSM=",
+                        "/pCTcDOekWzE9YZwQ4xHC83JUbbNMOfNkG25dQ/yoZI=", 
+                        "OfDChClZSy/N15Y31IKY9RZrQO3tzA/cw229amW0JXE=", 
+                        "o62odfgggADqujiYRMIjBfFqMmp0HPVHLYfBpeQfGTQ=", 
+                        "wHpuLagMalhAX+owv5o8i0E40VlMPRTiLiMIl5cMOnk=", 
+                        "gMu2UOcb9zbEL1b+djuY3YTJgRmHDlnx0DN4rVxd3qY=", 
+                        "jABXSFbX0AwgJ9rflJFwu0kHxK3luWr3FJCpKyndllw=", 
+                        "bUchNU+IBG8RZ2ddlq7+7AmwlIXLpNoOce+Qfq9KVKI="
                     ],
                     [
-                        "a1HUMd9dfxQcvs7M957fPdhhw7QGnwsRZho+76y7qRg=",
-                        "huUBSWWGYTEqngs1VY2E9sbT2nl/VSqWV/4FWMpAze8=",
-                        "doi271JVWWLQCP/4lCI1gsSEUXzqfaSe5ngArcf8iGY=",
-                        "NJxBIBti24URkmZcUEs1D/mMa0X7YqiiFh94tlNNjek="
+                        "8adjx5y3fXOlkqzLvb/LTSb8S5Htnzs6ITfbmoKz3Zs=",
+                        "hT3Ksf3Uzf6uzqE/oqZFMxI27pZ67CRg+o8rBrAYUHk=",
+                        "7Ss4sPbCPt1QpyUVgpHgjbb4bOErgZX4j6/o4361GHQ=",
+                        "Bi6wCoITshy8jY8JoN/eYIPk95qx1L8x2lBsFITCwNw="
                     ],
                     [
-                        "YmKvb8Ccw3BpXV3DyDkDDcCmE1FXOnuHvYa1J1gi1es=",
-                        "FTUPLddiAestCC4o7GPJhTSkDJSaawReXFo8XnsuzIc="
+                        "WSkLXusmgdkfgjl/pfKauq2PWNDODrmQrbs6TmpvSl8=",
+                        "xW9LYp4FVYwqkMuREDN+y1+wlb83BtvVJgnR0PTEVlE="
                     ],
                     [
-                        "idsOrW2kkzmGKyZ4lDt9M9ryXMLAnnC4JjF88tLyKRw="
+                        "0DbVwOouMaiGebP74WdVkLlrW95NdWIVbkwztkPxqG0="
                     ]
                 ]),
                 message: "hello world", // 1011
@@ -54,78 +91,79 @@ describe("MerkleLamportSigner", () => {
                 index: 0
             },
             output: {
-                signature: "u66w4gBAZbjcseTg:p1-2::p2-1:cd47u2FchVCX2F/4::6tTzGUW8Cy2d6OuB:p3-2::Eo57J2kXu71I6xSeiXG961xJc0vy4ZeIUTRUG9BF2zWjFjYhfB/G+rNj2+dN9WfVQ2kQNNzi6AdiS4srmOgC7g==:p4-2,2:huUBSWWGYTEqngs1VY2E9sbT2nl/VSqWV/4FWMpAze8=:FTUPLddiAestCC4o7GPJhTSkDJSaawReXFo8XnsuzIc="
+                signature: "Zkr6ceBStsK/jY1R:i0p2::i0p3:yFvfFWOHDl5IV8ci::FLkoqREiltsnt0Ys:i0p6::m5lOjM+LbdPcUp+fImJcGP37+D770pepFXFg6noqr2BhVw0M+arvg5Uqr/DK+ZqgXf5jvHHklll/4Zi+vsTZZw==:i0p8,/pCTcDOekWzE9YZwQ4xHC83JUbbNMOfNkG25dQ/yoZI=:hT3Ksf3Uzf6uzqE/oqZFMxI27pZ67CRg+o8rBrAYUHk=:xW9LYp4FVYwqkMuREDN+y1+wlb83BtvVJgnR0PTEVlE="
             }
         },
-        // Same as above except different index.
-        // Authentication path should be different in this case.
+        // Same as above but with different index
         {
             name: "basic-different-index",
             input: {
                 bitCount: 4,
                 lamportPrivateKeys: [
-                    "p1-1", "p1-2", "p2-1", "p2-2",
-                    "p3-1", "p3-2", "p4-1", "p4-2"
+                    [
+                        "i0p1", "i0p2", "i0p3", "i0p4",
+                        "i0p5", "i0p6", "i0p7", "i0p8"
+                    ],
+                    [
+                        "i1p1", "i1p2", "i1p3", "i1p4",
+                        "i1p5", "i1p6", "i1p7", "i1p8"
+                    ],
+                    [
+                        "i2p1", "i2p2", "i2p3", "i2p4",
+                        "i2p5", "i2p6", "i2p7", "i2p8"
+                    ],
+                    [
+                        "i3p1", "i3p2", "i3p3", "i3p4",
+                        "i3p5", "i3p6", "i3p7", "i3p8"
+                    ],
+                    [
+                        "i4p1", "i4p2", "i4p3", "i4p4",
+                        "i4p5", "i4p6", "i4p7", "i4p8"
+                    ],
+                    [
+                        "i5p1", "i5p2", "i5p3", "i5p4",
+                        "i5p5", "i5p6", "i5p7", "i5p8"
+                    ],
+                    [
+                        "i6p1", "i6p2", "i6p3", "i6p4",
+                        "i6p5", "i6p6", "i6p7", "i6p8"
+                    ],
+                    [
+                        "i7p1", "i7p2", "i7p3", "i7p4",
+                        "i7p5", "i7p6", "i7p7", "i7p8"
+                    ]
                 ],
                 merkleTree: new MerkleTree([
                     [
-                        "1", "2", "3", "4", "5", "6", "7", "8"
+                        "h5Y8vedTt03whfNJYmUDM00bExRkrIuZVMxbGLNYDSM=",
+                        "/pCTcDOekWzE9YZwQ4xHC83JUbbNMOfNkG25dQ/yoZI=", 
+                        "OfDChClZSy/N15Y31IKY9RZrQO3tzA/cw229amW0JXE=", 
+                        "o62odfgggADqujiYRMIjBfFqMmp0HPVHLYfBpeQfGTQ=", 
+                        "wHpuLagMalhAX+owv5o8i0E40VlMPRTiLiMIl5cMOnk=", 
+                        "gMu2UOcb9zbEL1b+djuY3YTJgRmHDlnx0DN4rVxd3qY=", 
+                        "jABXSFbX0AwgJ9rflJFwu0kHxK3luWr3FJCpKyndllw=", 
+                        "bUchNU+IBG8RZ2ddlq7+7AmwlIXLpNoOce+Qfq9KVKI="
                     ],
                     [
-                        "a1HUMd9dfxQcvs7M957fPdhhw7QGnwsRZho+76y7qRg=",
-                        "huUBSWWGYTEqngs1VY2E9sbT2nl/VSqWV/4FWMpAze8=",
-                        "doi271JVWWLQCP/4lCI1gsSEUXzqfaSe5ngArcf8iGY=",
-                        "NJxBIBti24URkmZcUEs1D/mMa0X7YqiiFh94tlNNjek="
+                        "8adjx5y3fXOlkqzLvb/LTSb8S5Htnzs6ITfbmoKz3Zs=",
+                        "hT3Ksf3Uzf6uzqE/oqZFMxI27pZ67CRg+o8rBrAYUHk=",
+                        "7Ss4sPbCPt1QpyUVgpHgjbb4bOErgZX4j6/o4361GHQ=",
+                        "Bi6wCoITshy8jY8JoN/eYIPk95qx1L8x2lBsFITCwNw="
                     ],
                     [
-                        "YmKvb8Ccw3BpXV3DyDkDDcCmE1FXOnuHvYa1J1gi1es=",
-                        "FTUPLddiAestCC4o7GPJhTSkDJSaawReXFo8XnsuzIc="
+                        "WSkLXusmgdkfgjl/pfKauq2PWNDODrmQrbs6TmpvSl8=",
+                        "xW9LYp4FVYwqkMuREDN+y1+wlb83BtvVJgnR0PTEVlE="
                     ],
                     [
-                        "idsOrW2kkzmGKyZ4lDt9M9ryXMLAnnC4JjF88tLyKRw="
+                        "0DbVwOouMaiGebP74WdVkLlrW95NdWIVbkwztkPxqG0="
                     ]
                 ]),
-                message: "hello world", // binary first 4 bits = 1011
+                message: "hello world", // 1011
                 privateKey: "PRIVATE_KEY",
                 index: 4
             },
             output: {
-                signature: "u66w4gBAZbjcseTg:p1-2::p2-1:cd47u2FchVCX2F/4::6tTzGUW8Cy2d6OuB:p3-2::Eo57J2kXu71I6xSeiXG961xJc0vy4ZeIUTRUG9BF2zWjFjYhfB/G+rNj2+dN9WfVQ2kQNNzi6AdiS4srmOgC7g==:p4-2,6:NJxBIBti24URkmZcUEs1D/mMa0X7YqiiFh94tlNNjek=:YmKvb8Ccw3BpXV3DyDkDDcCmE1FXOnuHvYa1J1gi1es="
-            }
-        },
-        // Different values
-        {
-            name: "different-values",
-            input: {
-                bitCount: 4,
-                lamportPrivateKeys: [
-                    "P1", "P2", "P3", "P4",
-                    "P5", "P6", "P7", "P8"
-                ],
-                merkleTree: new MerkleTree([
-                    [
-                        "A", "B", "C", "D", "E", "F", "G", "H"
-                    ],
-                    [
-                        "OBZPvRdgPXP2lri01yZk1zW7anyIV3aH/SrjP9aWQVM=",
-                        "kOxYEn7Ecv+34/kMPuMg+LsdxrxkpIFD5tkffZpt4jY=",
-                        "Ok207h5ZzhoKG59WvW1VBtjCBOLx1QG3o6QCHmNl6Ns=",
-                        "p3cMbaqkvcIttg4q0tYfipSxS4jkKGWpMmdGmrtMw2U="
-                    ],
-                    [
-                        "oGqueaKBduEiiv2corYo7YVGDlZJ8/M9RUGfVdyDAVQ=",
-                        "ZE3vKRywK0TitkILIIufHlKbslb++5ZR+/PgKCN8/WU="
-                    ],
-                    [
-                        "KsZEbXtnBZ5V27aZNRDRJ2vCDbIjJCVVashfVRJM3Vs="
-                    ]
-                ]),
-                message: "bbyou bbyou bbme", // binary first 4 bits = 1000
-                privateKey: "OTHER_PRIVATE_KEY",
-                index: 7
-            },
-            output: {
-                signature: "++rnwYZntph1GPOu:P2::P3:4FGEwfWAEqvuzAOT::P5:dx2gTBS8hGID8Er6::P7:jljIrAB5EOeB9fOFgTRntllR0REqR4oKxfMrN66mEL7KJQuNJnAkD+Dgg3gUraX4vmqlbbwTLu73jexUkzIdPg==,G:Ok207h5ZzhoKG59WvW1VBtjCBOLx1QG3o6QCHmNl6Ns=:oGqueaKBduEiiv2corYo7YVGDlZJ8/M9RUGfVdyDAVQ="
+                signature: "8JXWdDRRGpAyquix:i4p2::i4p3:7bNwxEvpsYenDTIB::e+e1oV/Pi+i8IZD6:i4p6::Zg+mVsWZMFwO5IY4jCKzrgk3XdIf1/JytNzna2AAxSPBPUmcl6alEVOL+x4ouHh3ffKDGysPBAkFlj1GlctEEQ==:i4p8,gMu2UOcb9zbEL1b+djuY3YTJgRmHDlnx0DN4rVxd3qY=:Bi6wCoITshy8jY8JoN/eYIPk95qx1L8x2lBsFITCwNw=:WSkLXusmgdkfgjl/pfKauq2PWNDODrmQrbs6TmpvSl8="
             }
         }
     ];
@@ -137,12 +175,15 @@ describe("MerkleLamportSigner", () => {
     it("should create correct signatures", () => {
         // We must mock the generated private keys
         let testVectorIndex = 0;
+        let testVectorSubIndex = 0;
         
         spyOn((<any>signer), "getLamportPrivateKeys").and.callFake(() => {
-            return testVectors[testVectorIndex].input.lamportPrivateKeys;
+            return testVectors[testVectorIndex].input.lamportPrivateKeys[testVectorSubIndex];
         });
 
         for(let testVector of testVectors) {
+            testVectorSubIndex = testVector.input.index;
+
             let signature = signer.getSignature(
                 testVector.input.merkleTree,
                 testVector.input.message,
