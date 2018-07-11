75b4c5426a04a88990e3ea5fc77b176ef15fbdab
Unknown <mail@mathyn.com>
2018-07-11 10:39:51 +0200

    Added unit tests to the TransactionSerializer

--- src/core/transactions/TransactionSerializer.ts
+++ src/core/transactions/TransactionSerializer.ts
@@ -3,13 +3,6 @@ import { ITransaction } from "../../models/ITransaction";
 export class TransactionSerializer {
     transactionToString(transaction: ITransaction): string {
         // Just a dummy....
-        return `
-            ${ transaction.timestamp.toString() }
-            ${ transaction.inputAddress }
-            ${ transaction.fee.toString() }
-            ${ transaction.dataHash }
-            ${ transaction.assetId }
-            ${ transaction.inputAmount.toString() }
-        `;
+        return `${ transaction.timestamp.toString() }:${ transaction.inputAddress }:${ transaction.fee.toString() }:${ transaction.dataHash }:${ transaction.assetId }:${ transaction.inputAmount.toString() }`;
     }
 }
