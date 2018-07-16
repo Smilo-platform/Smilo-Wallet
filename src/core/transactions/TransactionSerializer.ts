d781d266616db22001542bf4aaf48fb49873ee85
Unknown <mail@mathyn.com>
2018-07-16 15:14:43 +0200

    Rewrote TransactionSerializer to TransactionHelper

--- src/core/transactions/TransactionSerializer.ts
+++
@@ -1,8 +0,0 @@
-import { ITransaction } from "../../models/ITransaction";
-
-export class TransactionSerializer {
-    transactionToString(transaction: ITransaction): string {
-        // Just a dummy....
-        return `${ transaction.timestamp.toString() }:${ transaction.inputAddress }:${ transaction.fee.toString() }:${ transaction.dataHash }:${ transaction.assetId }:${ transaction.inputAmount.toString() }`;
-    }
-}
