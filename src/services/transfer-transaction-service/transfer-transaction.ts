13d300d552632a0ac89faf37118a609098b8551c
Unknown <mail@mathyn.com>
2018-07-17 16:11:42 +0200

    TransferTransactionService now connects to correct endpoint

--- src/services/transfer-transaction-service/transfer-transaction.ts
+++ src/services/transfer-transaction-service/transfer-transaction.ts
@@ -1,16 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { ITransaction } from "../../models/ITransaction";
+import { UrlService } from "../url-service/url-service";
 
 export interface ITransferTransactionService {
-    sendTransaction(transaction: ITransaction): Promise<Object>
+    sendTransaction(transaction: ITransaction): Promise<ITransaction>
 }
 
 @Injectable()
 export class TransferTransactionService implements ITransferTransactionService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient,
+                private urlService: UrlService) {}
 
-    sendTransaction(transaction: ITransaction): Promise<Object> {
-        return this.http.post("", transaction).toPromise();
+    sendTransaction(transaction: ITransaction): Promise<ITransaction> {
+        return this.http.post<ITransaction>(
+            `${ this.urlService.getBaseUrl() }/transaction`, 
+            transaction
+        ).toPromise();
     }
 }
