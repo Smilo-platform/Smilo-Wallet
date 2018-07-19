eb1b558f4a40442766a4c20ee2433d5c595b5cbe
Unknown <mail@mathyn.com>
2018-07-19 11:03:14 +0200

    Changed return type of sendTransaction

---
+++ src/models/IPostTransactionResult.ts
@@ -0,0 +1,4 @@
+export interface IPostTransactionResult {
+    succeeded: boolean;
+    error: string;
+}
--- src/services/transfer-transaction-service/transfer-transaction.ts
+++ src/services/transfer-transaction-service/transfer-transaction.ts
@@ -2,9 +2,10 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { ITransaction } from "../../models/ITransaction";
 import { UrlService } from "../url-service/url-service";
+import { IPostTransactionResult } from "../../models/IPostTransactionResult";
 
 export interface ITransferTransactionService {
-    sendTransaction(transaction: ITransaction): Promise<ITransaction>
+    sendTransaction(transaction: ITransaction): Promise<IPostTransactionResult>
 }
 
 @Injectable()
@@ -12,8 +13,8 @@ export class TransferTransactionService implements ITransferTransactionService {
     constructor(private http: HttpClient,
                 private urlService: UrlService) {}
 
-    sendTransaction(transaction: ITransaction): Promise<ITransaction> {
-        return this.http.post<ITransaction>(
+    sendTransaction(transaction: ITransaction): Promise<IPostTransactionResult> {
+        return this.http.post<IPostTransactionResult>(
             `${ this.urlService.getBaseUrl() }/tx`, 
             transaction
         ).toPromise();
