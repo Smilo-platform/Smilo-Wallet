d50b97624314bf2569758a6a7d8369102aec5cec
Unknown <mail@mathyn.com>
2018-07-20 08:39:13 +0200

    AddressService now correctly handles 404 errors

--- src/services/address-service/address-service.ts
+++ src/services/address-service/address-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { IAddress } from "../../models/IAddress";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { UrlService } from "../url-service/url-service";
 
 @Injectable()
@@ -13,6 +13,27 @@ export class AddressService {
     get(address: string): Promise<IAddress> {
         return this.httpClient.get<IAddress>(
             `${ this.urlService.getBaseUrl() }/address/${ address }`
-        ).toPromise();
+        ).toPromise().then<IAddress>(
+            (address) => address,
+            (error: HttpErrorResponse) => {
+                if(error.status == 404) {
+                    // 404 are expected when the address does not exist.
+                    // This is not a true error, we simply return an empty wallet object.
+                    let emptyAddress: IAddress = {
+                        publickey: address,
+                        contractBalanceMap: {
+                            "000x00123": 0
+                        },
+                        signatureCount: -1
+                    }
+
+                    return emptyAddress;
+                }
+                else {
+                    // All other status codes are true errors
+                    return <any>Promise.reject(error);
+                }
+            }
+        );
     }
 }
