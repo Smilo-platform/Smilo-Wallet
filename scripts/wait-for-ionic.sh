efcbc88ae23758a71ed7e6c4b845d46b025c91f6
Unknown <mail@mathyn.com>
2018-08-22 10:06:17 +0200

    wait for ionic script now skips any previous output to nohup.out

--- scripts/wait-for-ionic.sh
+++ scripts/wait-for-ionic.sh
@@ -1,7 +1,7 @@
 # This script will tail the content of 'nohup.out' until a line
 # containing the text 'dev server running' is found.
 # It will then kill the tail allowing other commands to run.
-tail -f ./nohup.out | while read LOGLINE
+tail -f -n0 ./nohup.out | while read LOGLINE
 do
     if [[ "${LOGLINE}" == *"dev server running"* ]]; then
         pkill -P $$ tail
