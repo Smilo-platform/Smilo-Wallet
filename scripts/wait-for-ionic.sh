0b07d75f720bc7bbb6df0a6edb3c0354ad42c8c1
Unknown <mail@mathyn.com>
2018-08-22 10:46:41 +0200

    Replaced wait-for-ionic script with wait-on module

--- scripts/wait-for-ionic.sh
+++
@@ -1,9 +0,0 @@
-# This script will tail the content of 'nohup.out' until a line
-# containing the text 'Development server running!' is found.
-# It will then kill the tail allowing other commands to run.
-tail -f -n0 ./nohup.out | while read LOGLINE
-do
-    if [[ "${LOGLINE}" == *"Development server running!"* ]]; then
-        pkill -P $$ tail
-    fi
-done
