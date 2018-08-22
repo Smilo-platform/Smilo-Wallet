97c06edd025a204fcf27acf51e0c338746a52ce0
Unknown <mail@mathyn.com>
2018-08-22 14:27:23 +0200

    Added E2E tests to the import private key page

--- cypress/integration/import-private-key-page.ts
+++ cypress/integration/import-private-key-page.ts
@@ -10,15 +10,34 @@ describe("ImportPrivateKeyPage", () => {
     it("should not show the import button initially", () => {
         goToPage();
 
-        
+        cy.get("page-wallet-import-privatekey").then(
+            (page) => {
+                expect(page.find("[data-cy=import-privatekey-button]")).to.have.length(0);
+            }
+        );
     });
 
     it("should show a password warning if the passwords do not match", () => {
         goToPage();
+        
+        cy.get("[data-cy=password-input] input").type("pass123");
+        cy.get("[data-cy=password-confirm-input] input").type("321ssap");
+
+        cy.get("[data-cy=password-error]");
     });
 
     it("should show the import button once all fields have been filled correctly", () => {
         goToPage();
+
+        cy.get("[data-cy=private-key-input] textarea").type("PrivateKey");
+        cy.get("[data-cy=wallet-name-input] input").type("Wallet Name");
+        cy.get("[data-cy=password-input] input").type("pass123");
+        cy.get("[data-cy=password-confirm-input] input").type("pass123");
+
+        cy.get("[data-cy=start-import-privatekey-button]").click();
+
+        // Prepare wallet page should now be shown
+        cy.get("page-prepare-wallet");
     });
 
     function goToPage() {
