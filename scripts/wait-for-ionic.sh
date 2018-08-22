165519968454406333168436739cbc3be48a0a29
Unknown <mail@mathyn.com>
2018-08-22 09:55:06 +0200

    Added script documentation

--- scripts/wait-for-ionic.sh
+++ scripts/wait-for-ionic.sh
@@ -1,3 +1,6 @@
+# This script will tail the content of 'nohup.out' until a line
+# containing the text 'dev server running' is found.
+# It will then kill the tail allowing other commands to run.
 tail -f ./nohup.out | while read LOGLINE
 do
     if [[ "${LOGLINE}" == *"dev server running"* ]]; then
